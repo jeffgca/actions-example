bcc27a70fc5d4853615be5ebd2fcb191921ccd75
Jeff Griffiths <jeff@eko-recordings.ca>
2021-01-10 10:23:56 -0800

    added many pings

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const _ = require('lodash');
+
 function ping(callback) {
   callback(null, 'pong!');
+}
+
+function manyPings(n, cb /* optional */ ) {
+  let pongs = _.map(_.range(n), (item) => {
+    return ['pong', item+1];
+  });
+  if (cb) { 
+    cb(null, pongs); 
+  }
+  else {
+    return pongs;
+  }
 }
 
 if (require.main === module) {
@@ -10,3 +24,4 @@ if (require.main === module) {
 }
 
 module.exports.ping = ping;
+module.exports.manyPings = manyPings;
--- test/ping.test.js
+++ test/ping.test.js
@@ -1,4 +1,4 @@
-let ping = require('../index').ping;
+let { ping, manyPings } = require('../index');
 
 test('ping', (done) => {
   ping((err, result) => {
@@ -6,4 +6,12 @@ test('ping', (done) => {
     expect(result).toBe('pong!');
     done();
   });
+});
+
+test('many pings', (done) => {
+  let pongs = manyPings(10);
+  console.log(pongs);
+  expect(pongs.length).toBe(10);
+  expect(pongs[9][1]).toBe(10);
+  done();
 });
